a572d43f5a94bb0999a66790f7a4ec665f61414c
Grimstoey <grimstoey@gmail.com>
2024-06-12 20:52:12 +0700

    Add project files.

---
+++ Rantory.DataAccess/ApplicationDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using Rantory.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rantory.DataAccess
+{
+    public class ApplicationDbContext : IdentityDbContext<User>
+    {
+        public DbSet<Story> Stories { get; set; }
+        public DbSet<Chapter> Chapters { get; set; }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+    }
+}
---
+++ Rantory.Models/Story.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Rantory.Models
+{
+    public class Story
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        [Display(Name = "Strat")]
+        public DateTime DateStart { get; set; }
+        [Required]
+        [Display(Name = "Finish")]
+        public DateTime DateFinish { get; set; }
+
+        public int UserId { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
+
+
+        ICollection<Chapter> Chapters { get; set; }
+
+    }
+}
